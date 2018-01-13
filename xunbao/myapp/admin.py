332f1310849896cc52fef52b6fd29b7a46abcebd
scorpion_ajay <aries.ajay1@outlook.com>
2018-01-13 16:50:21 +0530

    updated directory locations

--- xunbao/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Profile, Problems
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Problems)
--- xunbao/myapp/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class AnswerForm(forms.Form):
-    ans = forms.CharField(label='Answer here...', max_length=1000)
