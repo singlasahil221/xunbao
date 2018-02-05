000e3adb8e2dc74c38b1799a20c80d6a7d886182
Sahil Singla <singla.sahil221@gmail.com>
2018-02-05 10:56:12 +0530

    Update settings.py

--- xunbao/settings.py
+++ xunbao/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django'  # <- Here,
+    'social_django',  # <- Here,
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -97,6 +98,17 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
