a7dfa792847a0a6b176f444c3871bb7113890232
Sahil Singla <singla.sahil221@gmail.com>
2018-03-17 19:47:21 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
-    user = models.ForiegnKey(User, on_delete=models.CASCADE,many=True)
+    user = models.ForiegnKey(User, on_delete=models.CASCADE)
     solved = models.IntegerField(default=1)
     timetaken = models.DateTimeField(default=datetime.now)
 
