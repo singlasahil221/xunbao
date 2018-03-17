50f9deeb7a2baee7f5139fe4e2dfd8cf60423841
Sahil Singla <singla.sahil221@gmail.com>
2018-03-17 19:23:42 +0530

    Update serializers.py

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -9,14 +9,14 @@ class ProblemsSerializer(serializers.ModelSerializer):
         fields = ('pk','desc','image',)
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email',)
+	class Meta:
+		model = User
+		fields = ('email',)
 
 
 class LeaderboardSerializers(serializers.ModelSerializer):
-	user = serializers.SlugRelatedField(read_only=True,slug_field='first_name+last_name')
+	#user = serializers.SlugRelatedField(read_only=True,slug_field='first_name+last_name')
 	#last = serializers.SlugRelatedField(read_only=True,slug_field='last_name')
 	class Meta:
 		model = Profile
-		fields = ('user','solved')
+		fields = ('user.first_name + user.last_name','solved')
